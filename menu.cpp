38564b0ced909c041c1488291f74a87361d47715
wsalazarq <wsalazaq@gmail.com>
2024-06-24 08:39:45 -0500

    Creacion de las funciones e implementacion de la opcion depositar

--- menu.cpp
+++ menu.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void depositar(float);
+void retirar(float);
+void mostrarSaldo();
+
+float saldo = 0; //variable global
+
 int main(){
     int op;
+    float monto;
 
     cout<<" Cajero automatico "<<endl;
     cout<<" 1. Depositar "<<endl;
@@ -16,6 +23,11 @@ int main(){
         cout<<"Saliendo..."<<endl;
         system("pause");
         break;
+    case 1:
+        cout<<"Ingrese el monto a depositar: ";cin>>monto;
+        depositar(monto);
+        break;
+
     default:
         cout<<" Ingresa una opcion correcta "<<endl;
         system("pause");
@@ -24,4 +36,15 @@ int main(){
 
     return 0;
 
+}
+
+void depositar(float monto){
+    saldo = saldo + monto;
+    cout<<"El saldo es: "<<saldo<<endl;
+}
+void retirar(float monto){
+    saldo = saldo - monto;
+}
+void mostrarSaldo(){
+    cout<<"El saldo actual es: "<<saldo<<endl;
 }
