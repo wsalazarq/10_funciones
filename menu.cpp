9d70da4f88bb351e8e1f46ce64db7c549a26705f
wsalazarq <wsalazaq@gmail.com>
2024-06-24 08:49:15 -0500

    Implemetacion de las opciones: retirar y mostrarSaldo

--- menu.cpp
+++ menu.cpp
@@ -10,37 +10,49 @@ float saldo = 0; //variable global
 int main(){
     int op;
     float monto;
-
-    cout<<" Cajero automatico "<<endl;
-    cout<<" 1. Depositar "<<endl;
-    cout<<" 2. Retirar "<<endl;
-    cout<<" 3. Mostrar saldo "<<endl;
-    cout<<" 0. Salir "<<endl;
-    cout<<" Elija una opcion: ";cin>>op;
     
-    switch (op){
-    case 0:
-        cout<<"Saliendo..."<<endl;
-        system("pause");
-        break;
-    case 1:
-        cout<<"Ingrese el monto a depositar: ";cin>>monto;
-        depositar(monto);
-        break;
+    do{
+        cout<<" Cajero automatico "<<endl;
+        cout<<" 1. Depositar "<<endl;
+        cout<<" 2. Retirar "<<endl;
+        cout<<" 3. Mostrar saldo "<<endl;
+        cout<<" 0. Salir "<<endl;
+        cout<<" Elija una opcion: ";cin>>op;
+        
+        switch (op){
+        case 0:
+            cout<<"Saliendo..."<<endl;
+            system("pause");
+            break;
+        case 1:
+            cout<<"Ingrese el monto a depositar: ";cin>>monto;
+            depositar(monto);
+            system("pause");
+            break;
+        case 2:
+            cout<<"Ingrese el monto a retirar: ";cin>>monto;
+            retirar(monto);
+            system("pause");
+            break;
+        case 3:
+            mostrarSaldo();
+            system("pause");
+            break;
+        default:
+            cout<<" Ingresa una opcion correcta "<<endl;
+            system("pause");
+            break;
+        }
 
-    default:
-        cout<<" Ingresa una opcion correcta "<<endl;
-        system("pause");
-        break;
-    }
 
+    } while (op != 0);
+    
     return 0;
-
+    
 }
 
 void depositar(float monto){
     saldo = saldo + monto;
-    cout<<"El saldo es: "<<saldo<<endl;
 }
 void retirar(float monto){
     saldo = saldo - monto;
