dc91dcc0dc44ab22a257af1cdf05643c4208d9d9
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-26 17:35:00 -0500

    Actividad_1: Implementar la libreria cmath y calcular el area del circulo

--- actividad1.cpp
+++ actividad1.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main(){
-    int radio;
+    float radio, area;
     cout<<"Ingrese el radio del circulo: ";cin>>radio;
 
-    
+    area = M_PI * pow(radio,2);
+
+    cout<<"El area del circulo es: "<<area<<endl;
+
     return 0;
 }
