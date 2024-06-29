701c7bc955671a1fd8959c7f61153599d8461d9c
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 09:57:53 -0500

    Actividad_3: Implementar la libreria cmath y usar las funciones sqrt y pow

--- actividad3.cpp
+++ actividad3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main(){
@@ -7,6 +9,9 @@ int main(){
     cout<<"Ingrese el valor del primer cateto: ";cin>>c1;
     cout<<"Ingrese el valor del segundo cateto: ";cin>>c2;
 
-    
+    h = sqrt(pow(c1,2) + pow(c2,2));
+
+    cout<<"La hipotenusa es: "<< h <<endl;
+
     return 0;
 }
