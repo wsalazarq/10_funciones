80118feda6b29ddac6198fd990a08e65a9a3d1b1
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 09:51:58 -0500

    Agregar actividad_3

---
+++ actividad3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float c1, c2, h;
+    cout<<"Ingrese el valor del primer cateto: ";cin>>c1;
+    cout<<"Ingrese el valor del segundo cateto: ";cin>>c2;
+
+    
+    return 0;
+}
