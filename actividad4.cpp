c0906db766cf0d6361b56636eba27e36cba29ac7
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 11:09:27 -0500

    Actividad_4: Implementar la libreria cmath y validar si las raices son reales o complejas

--- actividad4.cpp
+++ actividad4.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(){
 
-    float a, b, c, d;
+    float a, b, c, d, x1,x2;
     cout<<"Ingrese el valor del coeficiente (a): ";cin>>a;
     cout<<"Ingrese el valor del coeficiente (b): ";cin>>b;
     cout<<"Ingrese el valor del coeficiente (c): ";cin>>c;
 
-    d = b * b - 4*a*c;
+    d = pow(b,2) - 4 * a * c;
+
+    if (d >= 0){
+        x1 = (-b + sqrt(d))/ (2 * a);                 
+        x2 = (-b - sqrt(d))/ (2 * a);   
+
+        cout<<"Raiz 1"<< x1 <<endl;
+        cout<<"Raiz 2"<< x2 <<endl;
+
+    }else{
+        cout<<"No tiene solucion en los reales";
+    }
 
     return 0;
 }
