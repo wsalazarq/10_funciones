3412c19707ea911843171d02ffb1349801a07c6b
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 09:40:41 -0500

    Agregar actividad_2

---
+++ actividad2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+
+    int max, cantidad, num;
+    cout<<"Ingrese el numero maximo: ";cin>>max;
+    cout<<"Ingrese los numeros que desea generar: ";cin>>cantidad;
+
+    for (int i = 0; i < cantidad; i++){
+        num  = rand() % max;
+        cout<<num<<endl;
+    }
+
+    return 0;
+}
