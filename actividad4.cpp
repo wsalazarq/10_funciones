d03b478206745cd592fcafa8506523c15c55e1ad
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 10:35:07 -0500

    Agregar actividad_4

---
+++ actividad4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float a, b, c, d;
+    cout<<"Ingrese el valor del coeficiente (a): ";cin>>a;
+    cout<<"Ingrese el valor del coeficiente (b): ";cin>>b;
+    cout<<"Ingrese el valor del coeficiente (c): ";cin>>c;
+
+    d = b * b - 4*a*c;
+
+    return 0;
+}
