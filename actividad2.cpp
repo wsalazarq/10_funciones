fad60de6100078f60015c8f0422fac95c60ed361
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 09:46:10 -0500

    Actividad_2: Implementar la libreria cstdlib para usar la funcion rand

--- actividad2.cpp
+++ actividad2.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int main(){
 
     int max, cantidad, num;
-    cout<<"Ingrese el numero maximo: ";cin>>max;
+    
     cout<<"Ingrese los numeros que desea generar: ";cin>>cantidad;
+    cout<<"Ingrese el numero maximo: ";cin>>max;
 
     for (int i = 0; i < cantidad; i++){
         num  = rand() % max;
