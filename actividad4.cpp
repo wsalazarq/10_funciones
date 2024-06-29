160a305100213603c695390c8c26669dfd847aed
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 11:14:34 -0500

    Actividad_4: Validar el coeficiente a

--- actividad4.cpp
+++ actividad4.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 int main(){
 
     float a, b, c, d, x1,x2;
+
     cout<<"Ingrese el valor del coeficiente (a): ";cin>>a;
     cout<<"Ingrese el valor del coeficiente (b): ";cin>>b;
     cout<<"Ingrese el valor del coeficiente (c): ";cin>>c;
 
-    d = pow(b,2) - 4 * a * c;
-
-    if (d >= 0){
-        x1 = (-b + sqrt(d))/ (2 * a);                 
-        x2 = (-b - sqrt(d))/ (2 * a);   
-
-        cout<<"Raiz 1"<< x1 <<endl;
-        cout<<"Raiz 2"<< x2 <<endl;
-
+    if (a == 0){
+        cout<<"El coeficiente a no puede ser 0";
     }else{
-        cout<<"No tiene solucion en los reales";
-    }
+        d = pow(b,2) - 4 * a * c;
 
+        if (d >= 0){
+            x1 = (-b + sqrt(d))/ (2 * a);                 
+            x2 = (-b - sqrt(d))/ (2 * a);   
+
+            cout<<"x1: "<< x1 <<endl;
+            cout<<"x2: "<< x2 <<endl;
+
+        }else{
+            cout<<"No tiene solucion en los reales";
+        }
+    }
+    
     return 0;
 }
