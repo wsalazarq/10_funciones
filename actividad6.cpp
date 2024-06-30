3cf1f73d8af18cb471af7e3902e90e1abb5b56a5
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-30 09:48:16 -0500

    Actividad_6: Implementar la funcion mostrarEleccion

--- actividad6.cpp
+++ actividad6.cpp
@@ -5,15 +5,33 @@ using namespace std;
 
 int eleccionUsu();
 int eleccionCompu();
+void mostrarEleccion(int);
 
 int main(){
     int usuario, computadora;
     usuario = eleccionUsu();
-    
+    cout<<"Usuario: ";
+    mostrarEleccion(usuario);
     
     computadora = eleccionCompu();
+    cout<<"Computadora: ";
+    mostrarEleccion(computadora);
     
     return 0;
+}
+
+void mostrarEleccion(int op){
+    switch (op){
+        case 1:
+        cout<<"Piedra"<<endl;
+            break;
+        case 2:
+        cout<<"Papel"<<endl;
+            break;   
+        case 3:
+        cout<<"Tijera"<<endl;
+            break;     
+        }
 }
 
 int eleccionUsu(){
