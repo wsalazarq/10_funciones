7a00eece08c3d8d5bf74e3b7409ff79a01dace0f
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-30 11:02:03 -0500

    Actividad_6: Actualizar el codigo para que funcione en 3 rondas y mostrar el ganador

--- actividad6.cpp
+++ actividad6.cpp
@@ -10,6 +10,9 @@ void ganadorRonda(int, int, int&, int&);
 
 int main(){
     int usuario, computadora, victoriaUsu = 0, victoriaCompu = 0;
+
+     while (victoriaUsu < 3 && victoriaCompu < 3){
+
         usuario = eleccionUsu();
         cout<<"Usuario: ";
         mostrarEleccion(usuario);
@@ -17,9 +20,15 @@ int main(){
         computadora = eleccionCompu();
         cout<<"Computadora: ";
         mostrarEleccion(computadora);
-
+        cout<<"===================================================="<<endl;
         ganadorRonda(usuario, computadora, victoriaUsu, victoriaCompu);
-        
+    }
+
+    if (victoriaUsu == 3){
+        cout<<"Ganaste el juego"<<endl;
+    }else{
+        cout<<"La computadora gano el juego"<<endl;
+    }    
     return 0;
 }
 
