56e03d3e456cbeb5f09a33141cb22efaed8f2ad2
Arturosq007 <126190714+Arturosq007@users.noreply.github.com>
2024-06-29 20:02:09 -0500

    Actividad 5: Implementacion de la funcion calcularRaices y asignar variables por referencia

--- actividad5.cpp
+++ actividad5.cpp
@@ -2,30 +2,45 @@
 #include <cmath>
 using namespace std;
 
-int main(){
+void calcularRaices(float, float, float, float&, float&, bool& );
 
-    float a, b, c, d, x1,x2;
+int main(){
+    float a,b,c,x1,x2;
+    bool sonReales;
 
     cout<<"Ingrese el valor del coeficiente (a): ";cin>>a;
     cout<<"Ingrese el valor del coeficiente (b): ";cin>>b;
     cout<<"Ingrese el valor del coeficiente (c): ";cin>>c;
-
+    
     if (a == 0){
-        cout<<"El coeficiente a no puede ser 0";
+        cout<<"El coeficiente (a) no puede ser 0 ";
     }else{
-        d = pow(b,2) - 4 * a * c;
-
-        if (d >= 0){
-            x1 = (-b + sqrt(d))/ (2 * a);                 
-            x2 = (-b - sqrt(d))/ (2 * a);   
-
+        calcularRaices(a, b, c, x1, x2, sonReales);
+        
+        if (sonReales){
             cout<<"x1: "<< x1 <<endl;
             cout<<"x2: "<< x2 <<endl;
-
         }else{
-            cout<<"No tiene solucion en los reales";
+            cout<<"Las raices son complejas y no tiene solucion en los reales";
         }
+        
     }
     
     return 0;
+}
+
+void calcularRaices(float a, float b, float c, float& x1, float& x2, bool& sonReales ){
+    float d;
+
+    d = pow(b,2) - 4 * a * c;
+
+    if (d >= 0){
+        sonReales = true;
+        x1 = (-b + sqrt(d))/ (2 * a);                 
+        x2 = (-b - sqrt(d))/ (2 * a);   
+
+    }else{
+        sonReales = false;
+    }
+     
 }
